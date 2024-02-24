77d71552c099d70670cbefe8da9f68d7adbbe8a2
Arnav Kohli <arnavkohli2003@gmail.com>
2024-02-24 09:42:36 +0000

    Complete 3, lab 1
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,48 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <_types/_uint32_t.h>
+#include <bitset>
+#include <cmath>
 
 //Constants
-  const uint32_t interval = 100; //Display update interval
+const uint32_t interval = 100; //Display update interval
+
 
 //Pin definitions
-  //Row select and enable
-  const int RA0_PIN = D3;
-  const int RA1_PIN = D6;
-  const int RA2_PIN = D12;
-  const int REN_PIN = A5;
+//Row select and enable
+const int RA0_PIN = D3;
+const int RA1_PIN = D6;
+const int RA2_PIN = D12;
+const int REN_PIN = A5;
 
-  //Matrix input and output
-  const int C0_PIN = A2;
-  const int C1_PIN = D9;
-  const int C2_PIN = A6;
-  const int C3_PIN = D1;
-  const int OUT_PIN = D11;
+//Matrix input and output
+const int C0_PIN = A2;
+const int C1_PIN = D9;
+const int C2_PIN = A6;
+const int C3_PIN = D1;
+const int OUT_PIN = D11;
 
-  //Audio analogue out
-  const int OUTL_PIN = A4;
-  const int OUTR_PIN = A3;
+//Audio analogue out
+const int OUTL_PIN = A4;
+const int OUTR_PIN = A3;
 
-  //Joystick analogue in
-  const int JOYY_PIN = A0;
-  const int JOYX_PIN = A1;
+//Joystick analogue in
+const int JOYY_PIN = A0;
+const int JOYX_PIN = A1;
 
-  //Output multiplexer bits
-  const int DEN_BIT = 3;
-  const int DRST_BIT = 4;
-  const int HKOW_BIT = 5;
-  const int HKOE_BIT = 6;
+//Output multiplexer bits
+const int DEN_BIT = 3;
+const int DRST_BIT = 4;
+const int HKOW_BIT = 5;
+const int HKOE_BIT = 6;
+
+const uint32_t stepSizes [] = {51149155, 54077542, 57396381, 60715219, 64424509,
+68133799, 72233540, 76528508, 81018701, 85899345, 90975216, 96441538};
+
+const char* notes[] = {"C", "C#", "D", "D#", "E", "F",
+                       "F#", "G", "G#", "A", "A#", "B"};
+
+volatile uint32_t currentStepSize = 0;
 
 //Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
@@ -45,6 +57,57 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,HIGH);
       delayMicroseconds(2);
       digitalWrite(REN_PIN,LOW);
+}
+
+std::bitset<4> readCols(){
+  std::bitset<4> result;
+
+  result[0] = !digitalRead(C0_PIN);
+  result[1] = !digitalRead(C1_PIN);
+  result[2] = !digitalRead(C2_PIN);
+  result[3] = !digitalRead(C3_PIN);
+
+  return result;
+}
+
+void setRow(uint8_t rowIdx){
+  digitalWrite(REN_PIN, LOW);
+  std::bitset<3> rowBits = std::bitset<3>(rowIdx);
+
+  digitalWrite(RA0_PIN, rowBits[0]);
+  digitalWrite(RA1_PIN, rowBits[1]);
+  digitalWrite(RA2_PIN, rowBits[2]);
+
+  digitalWrite(REN_PIN, HIGH);
+}
+
+std::bitset<32> setInputs(){
+  std::bitset<32> inputs;
+
+  for(int row = 0; row < 3; row++){
+    setRow(row);
+
+    delayMicroseconds(3);
+
+    std::bitset<4> colInput = readCols();
+
+    int startPos = row * 4;
+    for (int bit = 0; bit < 4; ++bit) {
+        inputs[startPos + bit] = colInput[bit];
+    }
+  }
+
+  return inputs;
+}
+
+void sampleISR(){
+  static uint32_t phaseAcc = 0;
+  uint32_t localCurrentStepSize = __atomic_load_n(&currentStepSize, __ATOMIC_RELAXED);
+  phaseAcc += currentStepSize;
+
+  int32_t Vout = (phaseAcc >> 24) - 128;
+
+  analogWrite(OUTR_PIN, Vout + 128);
 }
 
 void setup() {
@@ -77,12 +140,32 @@ void setup() {
   //Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
+
+  TIM_TypeDef *Instance = TIM1;
+  HardwareTimer *sampleTimer = new HardwareTimer(Instance);
+
+  sampleTimer->setOverflow(22000, HERTZ_FORMAT);
+  sampleTimer->attachInterrupt(sampleISR);
+  sampleTimer->resume();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   static uint32_t next = millis();
   static uint32_t count = 0;
+  std::bitset<32> inputs = setInputs();
+  const char* notePlayed = "Play a note...";
+
+  uint32_t localCurrentStepSize = 0;
+
+  for(int i = 0; i < 12; i++){
+    if(inputs[i]){
+      localCurrentStepSize = stepSizes[i];
+      notePlayed = notes[i];
+    }
+  }
+
+  __atomic_store_n(&currentStepSize, localCurrentStepSize, __ATOMIC_RELAXED);
 
   while (millis() < next);  //Wait for next interval
 
@@ -91,12 +174,13 @@ void loop() {
   //Update display
   u8g2.clearBuffer();         // clear the internal memory
   u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(count++);
+  u8g2.drawStr(2,10,"Hello World!");  // write something to the internal memory
+  // u8g2.setCursor(2,20);
+  // u8g2.print(inputs.to_ulong(),HEX);
+  u8g2.drawStr(2,20,notePlayed);
   u8g2.sendBuffer();          // transfer internal memory to the display
 
   //Toggle LED
   digitalToggle(LED_BUILTIN);
-  
+
 }
